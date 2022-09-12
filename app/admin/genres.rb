5ab442352851b46cd2891c11f2ff5c572b20bd8a
Andrey Sukhikh <andrey.sukhikh@jetruby.com>
2022-09-12 16:26:36 +0300

    Changed controller name & admin params permit

--- app/admin/genres.rb
+++ app/admin/genres.rb
@@ -1,7 +1,3 @@
 ActiveAdmin.register Genre do
-  permit_params do
-    permitted = [:title]
-    permitted << :other if params[:action] == 'create' && current_user.admin?
-    permitted
-  end
+  permit_params :title
 end

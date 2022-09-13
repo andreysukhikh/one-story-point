e4eb480d1ed9e43dc59239036f37fe69e215a871
Andrey Sukhikh <andrey.sukhikh@jetruby.com>
2022-09-13 10:42:33 +0300

    Added homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  get 'home/homepage'
   resources :books
   resources :authors
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
-  root 'books#index'
+  root 'home#homepage'
 end
